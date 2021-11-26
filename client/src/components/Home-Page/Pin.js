e02eb65def989d30fdf8c0f972ce53c8b7e94942
Gaurav <gauravburande2425@gmail.com>
2021-11-26 15:21:55 +0530

    Styling Changed

--- client/src/components/Home-Page/Pin.js
+++ client/src/components/Home-Page/Pin.js
@@ -1,61 +1,131 @@
-import React,{useState,useEffect}  from 'react'
-import './Pin.css';
-import FooterSticky from '../header-footer/FooterSticky';
-import FooterTwo from '../header-footer/FooterTwo';
+import React, { useState, useEffect } from "react";
+import "./Pin.css";
+import FooterSticky from "../header-footer/FooterSticky";
+import FooterTwo from "../header-footer/FooterTwo";
 
 function Pin() {
-    var loading=false;
-    
-    const names=["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40","41","42","43","44","45","46","47","48","49","50","51","52","53","54","55","56","57","58","59","60","61","62","64","66","2","35","17","37","8","29"].map((name,index)=>{
-        
-        return <img key={index} className={(index%2===0?"img card card_large":((index%3===0)?"img card card_medium":"img card card_small"))} alt={index} src={require(`../images/${name}.jpg`).default}/>
-    });
-    loading =true;
+  var loading = false;
 
-    // useEffect(() => {
-    //     document.onreadystatechange = function() {
-    //         if (document.readyState !== "complete") {
-    //             document.querySelector(
-    //               "#pintttt").style.visibility = "hidden";
-    //             document.querySelector(
-    //                 "#pintttt").style.display = "none";
-    //             document.querySelector(
-    //               "#loader").style.visibility = "visible";
-    //               setLoading(true);
-    //         } else {
-    //             document.querySelector(
-    //               "#loader").style.display = "none";
-    //             document.querySelector(
-    //               "body").style.visibility = "visible";
-    //         }
-    //     };
-       
-    // }, [loading])
-    
-
-  
+  const names = [
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17",
+    "18",
+    "19",
+    "20",
+    "21",
+    "22",
+    "23",
+    "24",
+    "25",
+    "26",
+    "27",
+    "28",
+    "29",
+    "30",
+    "31",
+    "32",
+    "33",
+    "34",
+    "35",
+    "36",
+    "37",
+    "38",
+    "39",
+    "40",
+    "41",
+    "42",
+    "43",
+    "44",
+    "45",
+    "46",
+    "47",
+    "48",
+    "49",
+    "50",
+    "51",
+    "52",
+    "53",
+    "54",
+    "55",
+    "56",
+    "57",
+    "58",
+    "59",
+    "60",
+    "61",
+    "62",
+    "64",
+    "66",
+    "2",
+    "35",
+    "17",
+    "37",
+    "8",
+    "29",
+  ].map((name, index) => {
     return (
+      <img
+        key={index}
+        className={
+          index % 2 === 0
+            ? "img card card_large"
+            : index % 3 === 0
+            ? "img card card_medium"
+            : "img card card_small"
+        }
+        alt={index}
+        src={require(`../images/${name}.jpg`).default}
+      />
+    );
+  });
+  loading = true;
 
+  // useEffect(() => {
+  //     document.onreadystatechange = function() {
+  //         if (document.readyState !== "complete") {
+  //             document.querySelector(
+  //               "#pintttt").style.visibility = "hidden";
+  //             document.querySelector(
+  //                 "#pintttt").style.display = "none";
+  //             document.querySelector(
+  //               "#loader").style.visibility = "visible";
+  //               setLoading(true);
+  //         } else {
+  //             document.querySelector(
+  //               "#loader").style.display = "none";
+  //             document.querySelector(
+  //               "body").style.visibility = "visible";
+  //         }
+  //     };
 
+  // }, [loading])
 
-        <>
-        <main>
-           
-       
+  return (
+    <>
+      <main>
         <div className="pin_containers1" id="#pintttt">
-         
-            {names}
-            
+          {names}
         </div>
-        </main>
-        <FooterSticky/>
-        {/* <FooterTwo/> */}
-    
+      </main>
+      <FooterSticky />
+      {/* <FooterTwo/> */}
     </>
-    )
+  );
 }
 
-export default Pin
-
-
-  
+export default Pin;
